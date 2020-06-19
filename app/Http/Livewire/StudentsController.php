c38b9ac919fd2bd8eda525a62842826b93474e50
Francisco Campos <franciiscocampos170@gmail.com>
2020-06-19 12:17:17 +0100

    store image to database

--- app/Http/Livewire/StudentsController.php
+++ app/Http/Livewire/StudentsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Student;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 
@@ -9,19 +10,32 @@ class StudentsController extends Component
 {
     use WithFileUploads;
 
-    public $photo;
+    public $name, $email, $photo;
 
     public function save()
     {
         $this->validate([
             'photo' => 'image|max:1024',
+            'name' => 'required',
+            'email' => 'required'
         ]);
+        
+        $name = $this->name;
+        $email = $this->email;
+        $file_name = md5($this->photo . microtime()). '.'.$this->photo->extension();
 
-        $this->photo->store('photos');
+        $this->photo->storeAs('photos', $file_name);
+
+        Student::create([
+            'name'=> $name,
+            'email'=>$email,
+            'photo'=> $file_name
+        ]);
     }
 
     public function render()
     {
         return view('livewire.students-controller');
     }
+
 }
