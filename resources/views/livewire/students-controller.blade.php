132510a7a352f833176744e16476d415e7527673
Francisco Campos <franciiscocampos170@gmail.com>
2020-06-19 12:17:28 +0100

    add name and email form to view

--- resources/views/livewire/students-controller.blade.php
+++ resources/views/livewire/students-controller.blade.php
@@ -1,5 +1,8 @@
 <div>
     <form action="" method="post" wire:submit.prevent="save">
+        <input type="text" wire:model='name'>
+        <input type="text" wire:model='email'>
+        
         <input type="file" id="" wire:model="photo" class="rounded-lg bg-white border border-gray-500 text-blue-500 shadow-lg px-4 py-2 mt-2 placeholder-indigo-300">
         
         <p class="text-red-500 text-xs italic my-4">@error('photo') <span class="error">{{ $message }}</span> @enderror</p>
