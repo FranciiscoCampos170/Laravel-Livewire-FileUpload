a43ecf396937b40281e747bf9d9a0447160faa10
Francisco Campos <franciiscocampos170@gmail.com>
2020-06-19 13:00:27 +0100

    refractoring all views

--- resources/views/livewire/students-controller.blade.php
+++ resources/views/livewire/students-controller.blade.php
@@ -1,8 +1,8 @@
 <div>
     <form action="" method="post" wire:submit.prevent="save">
-        <input type="text" wire:model='name'>
-        <input type="text" wire:model='email'>
-        
+        <input type="text" wire:model='name' placeholder='name' class="rounded-lg bg-white border border-gray-500 text-blue-500 shadow-lg px-4 py-2 mt-2 placeholder-indigo-300">
+        <input type="email" wire:model='email' placeholder='email' class="rounded-lg bg-white border border-gray-500 text-blue-500 shadow-lg px-4 py-2 mt-2 placeholder-indigo-300">
+
         <input type="file" id="" wire:model="photo" class="rounded-lg bg-white border border-gray-500 text-blue-500 shadow-lg px-4 py-2 mt-2 placeholder-indigo-300">
         
         <p class="text-red-500 text-xs italic my-4">@error('photo') <span class="error">{{ $message }}</span> @enderror</p>
@@ -10,3 +10,6 @@
         <button type="submit" class="rounded-lg px-4 py-2 mt-2 bg-blue-900 text-white">Save</button>
     </form>
 </div>
+
+<div>
+
